f4712e1bbc3876c53af80c2a4d2a3128c45dea0f
Dev-Gitanjali <jgitanjali592@gmail.com>
2024-02-24 10:44:18 +0530

    this method for format number of money

--- script.js
+++ script.js
@@ -50,5 +50,12 @@ function updateDOM(providedData = data) {
     });
   }
 
+  // Format number as money - https://stackoverflow.com/questions/149055/how-to-format-numbers-as-currency-string
+function formatMoney(number) {
+    return '$' + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+  }
+  
 
+// event listener
+addUserBtn.addEventListener('click', getRandomUser);
 
